006800c43eed85c70f62e157ba2376623be02a23
Brejcha13320 <davidmejia-2000@outlook.com>
2022-08-22 12:59:15 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgChartsModule } from 'ng2-charts';
+
+import { AppComponent } from './app.component';
+import { LineaComponent } from './components/linea/linea.component';
+import { BarrasComponent } from './components/barras/barras.component';
+import { DonaComponent } from './components/dona/dona.component';
+import { RadarComponent } from './components/radar/radar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LineaComponent,
+    BarrasComponent,
+    DonaComponent,
+    RadarComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgChartsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
